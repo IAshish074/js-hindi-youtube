53b06beedb0971b60f5b25cacfca96f2e54c72aa
IAshish074 <160568742+IAshish074@users.noreply.github.com>
2024-09-14 10:15:39 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -11,12 +11,12 @@ let userEmail
 const id =Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id===anotherId) // false
+// console.log(id===anotherId) // false
 
 
 const bigNumber = 5234524526262564352n
 
-console.log(bigNumber)
+// console.log(bigNumber)
 
 //Reference Type/Non-Primitive
 
@@ -32,13 +32,46 @@ let myobj={
 
 
 const myfunction=function(){
-    console.log("Hello world")
+    // console.log("Hello world")
 }
 
-console.log(typeof bigNumber) // bigint
-console.log(typeof heros) // object
-console.log(typeof outsideTemp)// object
-console.log(typeof myobj) // object
-console.log(typeof myfunction) // function
+// console.log(typeof bigNumber) // bigint
+// console.log(typeof heros) // object
+// console.log(typeof outsideTemp)// object
+// console.log(typeof myobj) // object
+// console.log(typeof myfunction) // function
+
+
+
+
+// **********************************************
+
+
+//stack(primitive) ,Heap(non-primitive) memory
+
+let myYouTubename ="Ashish Kumar Mishra"
+
+
+let anotherName= myYouTubename
+
+anotherName = "amishra"
+
+console.log(anotherName)
+console.log(myYouTubename)
+
+
+let user1 ={
+
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let user2 = user1
+
+
+user2.email= "amishrahsm@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
 
 
