e4c9180806ad50b9bcade83ac45f009dd4b15d95
IAshish074 <160568742+IAshish074@users.noreply.github.com>
2024-09-14 10:54:14 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,49 @@
+const name = "Ashish"
+const repocount = 50
+
+// console.log(name + repocount + " value");
+
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+
+const gameName = new String ('Ashish Kumar Mishra')
+
+
+console.log(gameName[0])
+console.log(gameName.__proto_);
+
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt[2])
+
+
+const newString=gameName.substring(0,4) // last - 1
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+
+const newString1 = "    Ashish   "
+console.log(newString1);
+console.log(newString1.trim())
+
+
+const url = "https://Ashish.com/Ashish%20Mishra"
+
+console.log(url.replace('%20','-'))
+
+
+console.log(gameName.split(' '))
+
+
+
+
+
+
+
+
