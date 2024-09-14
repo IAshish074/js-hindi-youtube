d871184c714a3586fae575712ef9a50d496a414c
IAshish074 <160568742+IAshish074@users.noreply.github.com>
2024-09-14 07:10:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,33 @@
+const accoundId = 144553
+let accountEmail = "amishrahsm@gmail.com"
+var accountPassword = "12345"
+accontCity = "jaipur"
+
+let accountState // if we declare a variable and not assign any value then output is --- output --> undefined
+/*
+to declare a constant we use const keyword
+
+but to declare a variable we use let and var keyword
+
+var keyword have scope problem so we use let keyword
+
+*/
+
+// const keyword= value is fixed once assigned 
+
+//accountId =2 -- will not change / not allowed
+
+accountEmail="ashish1996@gmail.com"
+accountPassword="212121"
+accountCity="delhi"
+
+console.log(accoundId) 
+console.table([accoundId,accountEmail,accountPassword,accountCity,accountState])
+//console.table used to see data in table format
+
+/*
+prefer not to use var
+
+because of isse in block scope and functional scope
+*/
+
