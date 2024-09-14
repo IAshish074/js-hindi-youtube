9e2a6f7eb34f5bce3c2e9387ffad9c0c32a33e33
IAshish074 <160568742+IAshish074@users.noreply.github.com>
2024-09-14 17:33:08 +0000

    array2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,44 @@
+const marvel_heroes = ["thor","ironman","spiderman"]
+
+const dc_heroes = ["superman","flash","batman"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes)
+// console.log(marvel_heroes[3][1])
+
+const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes);
+
+const all_new_Heroes = [...marvel_heroes, ...dc_heroes]
+
+// console.log(all_new_Heroes)
+
+
+
+const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+
+const real_another_array = anotherArray.flat(Infinity)
+// console.log(real_another_array)
+
+
+
+
+console.log(Array.isArray("Ashish"))
+console.log(Array.from("Ashish"))
+console.log(Array.from({name : "Ashish"})); // interesting
+
+
+
+let score1 = 100
+
+let score2 = 200
+
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3)) convert into array
+
+
+
+
