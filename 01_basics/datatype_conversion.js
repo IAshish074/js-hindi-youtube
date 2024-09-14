1832b1f52debede98c2d0de82aa577bd1a6aab41
IAshish074 <160568742+IAshish074@users.noreply.github.com>
2024-09-14 07:48:41 +0000

    conversion and operations

--- 01_basics/datatype_conversion.js
+++ 01_basics/datatype_conversion.js
@@ -1,58 +1,115 @@
+// /*
+// convrsions and operations
+// */
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+
+// let valueInNumber = Number(score)// convert the value in number(int)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) // the value is NaN(not a number) since 33abc is not a number type
+
+
+// let score2= null
+// console.log(typeof score2);
+// console.log(typeof(score2)); 
+
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2)
+// console.log(score2)
+
+
+
+// let score3 = undefined
+// console.log(typeof score3);
+// console.log(typeof(score3)); 
+
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3)
+// console.log(score3)
+
+
+// /*
+
+// "33" => 33
+// "33abc" => NaN
+// true=>1
+// false=>0
+
+// 1=>true
+// 0=>false
+
+// ""=>false
+// "Ashish"=>true
+//  */
+
+// let isLoggedIn = "Ashish"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+/**********************. operations ************************* */
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue); //-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Ashish"
+// console.log(str1+str2);
+
+
+// console.log("1" + 2) //12
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+// console.log(+true) //1
+// console.log(+"");//0
+
+
+let num1 , num2 , valueInNumber3
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
 /*
-convrsions and operations
+
+link to study
+
+https://262.ecma-international.org/15.0/index.html?_gl=1*oz2k38*_ga*Mzk4MzY0MTQwLjE3MjYyOTY3ODI.*_ga_TDCK4DWEPP*MTcyNjMwMDAyMS4yLjAuMTcyNjMwMDAyMS4wLjAuMA..#sec-type-conversion
+
 */
-let score = "33abc"
-
-console.log(typeof score);
-console.log(typeof(score)); 
-
-let valueInNumber = Number(score)// convert the value in number(int)
-
-console.log(typeof valueInNumber);
-console.log(valueInNumber) // the value is NaN(not a number) since 33abc is not a number type
-
-
-let score2= null
-console.log(typeof score2);
-console.log(typeof(score2)); 
-
-let valueInNumber2 = Number(score2)
-console.log(typeof valueInNumber2);
-console.log(valueInNumber2)
-console.log(score2)
 
 
 
-let score3 = undefined
-console.log(typeof score3);
-console.log(typeof(score3)); 
-
-let valueInNumber3 = Number(score3)
-console.log(typeof valueInNumber3);
-console.log(valueInNumber3)
-console.log(score3)
 
 
-/*
-
-"33" => 33
-"33abc" => NaN
-true=>1
-false=>0
-
-1=>true
-0=>false
-
-""=>false
-"Ashish"=>true
- */
-
-let isLoggedIn = "Ashish"
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+
+
+
