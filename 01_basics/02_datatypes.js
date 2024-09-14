d078506779cab98e9d36299621229a205395544f
IAshish074 <160568742+IAshish074@users.noreply.github.com>
2024-09-14 07:10:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all js code as newer version of javascript
+
+// alert(3+3) // we are using node js and not browser
+
+
+console.log("Ashish")
+
+
+let name = "Ashish"
+let age=21
+let isLoggedIN=false
+let state=null
+
+
+
+//number => 2 to pow 53--> range
+//bigint => if number is larger we use this
+//string => double cote is prefer
+//boolean =>true/false
+//null=>stand alone value
+//undefined=>when value is not defined
+//symbol=>to find uniqueness
+
+
+//object
+
+
+console.log(typeof null); 
+
+//type of null is object as output and type of undefined in undefined only
