f26bcb95a115acfb1ec36e0920647d19c3c34352
Ashish Kumar Mishra <160568742+IAshish074@users.noreply.github.com>
2024-09-15 07:08:58 +0000

    scope02

---
+++ functionsJs/02_scope.js
@@ -0,0 +1,57 @@
+// // var c = 300
+// {
+//     //scope
+
+//     if(true){
+//         let a = 10
+//         const b = 20
+
+//     }
+// }
+
+// // console.log(a);
+// // console.log(b);
+
+// console.log(c);
+
+// child functions can access their parent functions
+
+function one(){
+
+    const userName = "Ashish"
+
+    function two (){
+        const website = "youtube"
+        // console.log(userName)
+    }
+    // console.log(website)
+    // two()
+}
+// one()
+
+
+if(true){
+    const username = "Ashish"
+    if(username === "Ashish"){
+        const website = " youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+
+// +++++++++++++ interesting +++++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))
+
+
